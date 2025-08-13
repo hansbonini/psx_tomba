a3571beca7ee33ee62282f1d575278f164d9d775
hansbonini <hans@ysolutions.com.br>
2025-08-13 02:09:31 -0300

    Add some matches to PsyQ `libc.c`

--- src/scus_942.36/psyq/libc.c
+++ src/scus_942.36/psyq/libc.c
@@ -4,9 +4,57 @@ extern u32 D_8009B138;
 extern u32 D_8009B140;
 extern u8 D_8007FA71[];
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libc", _memcpy);
+u8* _memcpy(u8* arg0, u8* arg1, s32 arg2)
+{
+    s32 var_a2;
+    u8 temp_v0;
+    u8* temp_v1;
+    u8* var_a0;
+    u8* var_a1;
+    u8* var_v0;
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libc", _memset);
+    var_a0 = arg0;
+    var_a1 = arg1;
+    var_a2 = arg2;
+    var_v0 = NULL;
+    if (var_a0 != NULL) {
+        temp_v1 = var_a0;
+        if (var_a2 > 0) {
+            do {
+                temp_v0 = *var_a1;
+                var_a1 += 1;
+                var_a2 -= 1;
+                *var_a0 = temp_v0;
+                var_a0 += 1;
+            } while (var_a2 > 0);
+        }
+        var_v0 = temp_v1;
+    }
+    return var_v0;
+}
+
+s8* _memset(s8* arg0, s8 arg1, s32 arg2)
+{
+    s32 var_a2;
+    s8* var_a0;
+    s8* var_v0;
+
+    var_a0 = arg0;
+    var_a2 = arg2;
+    var_v0 = NULL;
+    if (var_a0 != NULL) {
+        var_v0 = var_a0;
+        if (var_a2 <= 0) {
+            return NULL;
+        }
+        do {
+            *var_a0 = arg1;
+            var_a2 -= 1;
+            var_a0 += 1;
+        } while (var_a2 > 0);
+    }
+    return var_v0;
+}
 
 int rand(void) {
     u_long nNext;
@@ -20,7 +68,29 @@ void srand(u_long seed) {
     D_8009B138 = seed;
 }
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libc", strcat);
+char* strcat(char* arg0, char* arg1)
+{
+    char* temp_v1;
+    char temp_v0;
+
+    if (arg0 == NULL) {
+        return NULL;
+    }
+    if (arg1 == NULL) {
+        return NULL;
+    }
+    if (&arg0[strlen(arg0)] != &arg1[strlen(arg1)]) {
+        temp_v1 = arg0;
+        while (*arg0++ != '\0') {}
+        arg0--;
+        do {
+            *arg0++ = temp_v0 = *arg1++;
+        } while (temp_v0 != '\0');
+        return temp_v1;
+    }
+
+    return NULL;
+}
 
 INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libc", strcmp);
 
