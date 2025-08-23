eead6962b5c5c46559b3e954b5f4cc0ab847c75b
hansbonini <hans@ysolutions.com.br>
2025-08-23 19:53:50 -0300

    Update PsyQ `libcd.c` symbols

--- src/scus_942.36/psyq/libcd.c
+++ src/scus_942.36/psyq/libcd.c
@@ -1,153 +1,129 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80064634);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", StSetRing);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80064664);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CdInit);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_800646F4);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", def_cbsync);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_8006471C);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", def_cbready);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80064744);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", def_cbread);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_8006476C);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", DeliverEvent);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_8006477C);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CdStatus);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_8006478C);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CdMode);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_8006479C);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CdLastCom);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_800647AC);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CdLastPos);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_800647BC);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CdReset);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80064828);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CdFlush);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80064848);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CdSetDebug);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80064860);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CdComstr);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80064894);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CdIntstr);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_800648C8);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CdSync);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_800648E8);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CdReady);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80064908);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CdSyncCallback);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80064920);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CdReadyCallback);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80064938);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CdControl);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80064A70);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CdControlF);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80064B9C);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CdControlB);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80064CE0);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CdMix);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80064D00);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CdGetSector);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80064D20);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CdDataCallback);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80064D44);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CdDataSync);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80064D64);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CdIntToPos);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80064E68);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CdPosToInt);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80064EE8);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", getintr);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80065470);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CD_sync);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_800656F0);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CD_ready);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_800659BC);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CD_cw);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80065DD8);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CD_vol);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80065E60);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CD_flush);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80065F40);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CD_initvol);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80066034);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CD_initintr);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80066084);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CD_init);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80066270);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CD_datasync);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_800663DC);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CD_getsector);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_800664DC);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CD_set_test_parmnum);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_800664EC);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", callback);
 
 INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", puts);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80066620);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", cb_read);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80066834);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", cd_read_retry);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80066A00);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CdReadBreak);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80066A50);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CdRead);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80066B30);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CdReadSync);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80066BFC);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CdReadCallback);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80066C14);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", CdRead2);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80066CA8);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", StCdInterrupt2);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80066CC8);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", StClearRing);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80066D28);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", StUnSetRing);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80066D80);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", data_ready_callback);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80066E10);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", StGetBackloc);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80066E70);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", StSetStream);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80066EF8);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", StFreeRing);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80066FB0);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", init_ring_status);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80066FEC);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", StGetNext);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_800670B0);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", StSetMask);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_800670D0);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", StCdInterrupt);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80067A40);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", mem2mem);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80067A74);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", dma_execute);
 
 INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", VSync);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80067D78);
-
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80067E14);
-
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", ResetCallback);
-
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80067E54);
-
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", DMACallback);
-
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80067EB4);
-
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80067EE8);
-
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80067F18);
-
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80067F48);
-
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80067F78);
-
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80067F88);
-
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", SetIntrMask);
-
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", func_80067FBC);
+INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd", v_wait);
