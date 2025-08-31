6f434f17b6ce109b4d4ca7d2c9a3b4ae1d65acd1
hansbonini <hans@ysolutions.com.br>
2025-08-31 19:35:10 -0300

    Add `strcmp` match to PsyQ `libc2`

--- src/scus_942.36/psyq/libc2/strcmp.c
+++ src/scus_942.36/psyq/libc2/strcmp.c
@@ -1,3 +1,19 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libc2/strcmp", strcmp);
+// INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libc2/strcmp", strcmp);
+int strcmp(const char *a, const char *b)
+{
+    if ((a == NULL) || (b == NULL)) {
+        if (a == b) {
+            return 0;
+        }
+        return (a == NULL) ? -1 : 1;
+    }
+    while (*a == *b++) {
+        if (*a++ == '\0') {
+            return 0;
+        }
+    }
+    b--;
+    return *a - *b;
+}
