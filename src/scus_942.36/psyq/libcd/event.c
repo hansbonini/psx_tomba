5b2de5164897189fd4b90fb5bfe36ba51aff6e66
hansbonini <hans@ysolutions.com.br>
2025-08-31 23:10:12 -0300

    Add `CdInit` match to PsyQ `libcd`

--- src/scus_942.36/psyq/libcd/event.c
+++ src/scus_942.36/psyq/libcd/event.c
@@ -1,7 +1,32 @@
 #include "common.h"
+#include "psyq/libcd.h"
 #include "psyq/kernel.h"
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd/event", CdInit);
+void def_cbsync(u_char intr, u_char* result);
+void def_cbready(u_char intr, u_char* result);
+void def_cbread(u_char intr, u_char* result);
+extern char* D_80015F14;
+
+// INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd/event", CdInit);
+int CdInit(void) {
+    int status;
+    int i;
+    i = 4;
+    while(1) {
+        status = CdReset(1);
+        i+=-1;
+        if (status == 1) break;
+        status = -1;
+        if (i == status) {
+           printf(&D_80015F14);
+          return 0;
+        }
+    }
+    CdSyncCallback(def_cbsync);
+    CdReadyCallback(def_cbready);
+    CdReadCallback(def_cbread);
+    return 1;
+}
 
 // INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd/event", def_cbsync);
 void def_cbsync(u_char intr, u_char* result) {
