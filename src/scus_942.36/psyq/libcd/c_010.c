121047e991b5899bb26d22cfb0aa35d77cd29945
hansbonini <hans@ysolutions.com.br>
2025-09-14 04:27:21 -0300

    Add `StSetMask` match to PsyQ `libcd/c_010`

--- src/scus_942.36/psyq/libcd/c_010.c
+++ src/scus_942.36/psyq/libcd/c_010.c
@@ -1,3 +1,14 @@
 #include "common.h"
+#include "psyq/libcd.h"
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd/c_010", StSetMask);
+extern u_long D_8009C960;
+extern u_long D_800A302C;
+extern u_long D_800A3060;
+
+// INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd/c_010", StSetMask);
+void StSetMask(u_long mask, u_long start, u_long end)
+{
+    D_800A3060 = mask;
+    D_8009C960 = start;
+    D_800A302C = end;
+}
