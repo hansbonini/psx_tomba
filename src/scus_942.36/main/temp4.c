3cf05dd2bbec5e794f13ebf02f23d498169abc7b
hansbonini <hans@ysolutions.com.br>
2025-09-30 03:49:11 -0300

    Add `func_800307AC` match

--- src/scus_942.36/main/temp4.c
+++ src/scus_942.36/main/temp4.c
@@ -35,7 +35,12 @@ INCLUDE_ASM("asm/scus_942.36/nonmatchings/main/temp4", func_80030734);
 
 INCLUDE_ASM("asm/scus_942.36/nonmatchings/main/temp4", func_8003076C);
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/main/temp4", func_800307AC);
+// INCLUDE_ASM("asm/scus_942.36/nonmatchings/main/temp4", func_800307AC);
+void func_800307AC(s32 arg0, s32 arg1, s32 arg2)
+{
+    func_80030800(arg0, arg1, arg2, 1, -1);
+    return;
+}
 
 // INCLUDE_ASM("asm/scus_942.36/nonmatchings/main/temp4", func_800307D4);
 void func_800307D4(s32 arg0, s32 arg1, s32 arg2, s16 arg3)
