928e1dbca1bb34403d9fca44511b3e7167588f8b
hansbonini <hans@ysolutions.com.br>
2025-10-07 22:19:56 -0300

    Add `strncat` match to PysQ `libc2`

--- src/scus_942.36/psyq/libc2/strncat.c
+++ src/scus_942.36/psyq/libc2/strncat.c
@@ -1,3 +1,23 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libc2/strncat", strncat);
+// INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libc2/strncat", strncat);
+u_char* strncat(u_char* dest, u_char* src, int count) {
+    u_char* temp;
+    
+    if (dest == NULL) {
+        return NULL;
+    } else {
+        temp = dest;
+        if (src == NULL) return NULL;
+        while (*dest++) {
+        }
+        dest--;
+        while ((*dest++ = *src++) != 0) {
+            if (--count < 0) {
+                dest[-1] = '\x00';
+                break;
+            }   
+        }
+    }
+    return temp;
+}
