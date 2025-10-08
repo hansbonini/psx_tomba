fbcc52c743f74c5775419ccafee3ad62faedbc0e
hansbonini <hans@ysolutions.com.br>
2025-10-08 03:09:14 -0300

    Add `init_ring_status` match to PysQ `libcd`

--- src/scus_942.36/psyq/libcd/c_008.c
+++ src/scus_942.36/psyq/libcd/c_008.c
@@ -1,3 +1,16 @@
 #include "common.h"
+#include "psyq/libcd.h"
 
-INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd/c_008", init_ring_status);
+extern StHEADER* D_800A326C;
+
+// INCLUDE_ASM("asm/scus_942.36/nonmatchings/psyq/libcd/c_008", init_ring_status);
+void init_ring_status(int arg0, u_long arg1)
+{
+    u_long i;
+    StHEADER* temp_v0;
+
+    for (i=0; i < arg1; i++){
+        temp_v0 = &D_800A326C[i+arg0];
+        *(int*)temp_v0 = 0;
+    }
+}
